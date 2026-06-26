139fdab95c1b43b6968ac64527442adc28bd3108
vivekananda-2201 <vivekananda2201s7@gmail.com>
2026-06-26 18:16:14 +0530

    Delete frontend directory

--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { RouterProvider } from 'react-router';
-import { router } from './routes';
-import './styles/theme.css';
-
-export default function App() {
-  return <RouterProvider router={router} />;
-}
